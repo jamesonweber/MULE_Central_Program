8f5c824d737dd39163ebd4854e3d7bf047619a3b
Jameson <jameson_weber@hotmail.com>
2016-12-12 11:12:01 -0700

    Inital Commit
    Added main python file, and template main function

---
+++ CentralProgram.py
@@ -0,0 +1,19 @@
+############################################################
+# Author:		Jameson Weber
+# Date:			December 12 2016
+# Description:	Central Program on the MULE. 
+#				This program is to deal with the input / ouput of controls to the user
+############################################################
+# Modification Log
+############################################################
+# Author:		
+# Date:			
+# Description:	
+############################################################
+
+def main():
+    print("Starting Central Program")
+
+
+if __name__ == "__main__":
+	main()
