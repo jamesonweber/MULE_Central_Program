07907f3a86584baa1650b711ffd996579cb8386e
Jameson <jameson_weber@hotmail.com>
2017-03-04 15:19:12 -0700

    setting up tcp connection

--- DataProgram.py
+++ DataProgram.py
@@ -2,7 +2,7 @@
 # References	
 ############################################################	
 # - https://wiki.python.org/moin/TcpCommunication		
-#
+# - http://stackoverflow.com/questions/415511/how-to-get-current-time-in-python
 ############################################################
 
 ############################################################
@@ -22,9 +22,11 @@
 # All required imports
 import sys
 import socket
-import serial
+#import serial
 import os
 import sys
+from datetime import datetime as dt
+import time
 
 
 # Main function definition
@@ -39,27 +41,34 @@ def main():
 	buff = 1024
 	print("Starting Data Program")
 	print("Attempting to open server [" + host + "] at port " + str(port))
-	while 1:
-		runServer(host, port, buff)
+	runServer(host, port, buff)
 	
 
 # Function to deal with client sending information to server
 def runServer(host, port, buff):
-#	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#	s.bind((host, port))
-#	s.listen(1)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((host, port))
+	s.listen(1)
 	print("Server Started")
-#	conn, addr = s.accept()
-	ser = serial.Serial('/dev/ttyACM0', 9600)
+	conn, addr = s.accept()
+#	ser = serial.Serial('/dev/ttyACM0', 9600)
 	print("Module Arduino Connected")
 	try: 
 		while 1:
-			data = ser.readline()
+#			data = ser.readline()
+			data = "DP|MULE|DISTANCE|0x1|1|CM|6.2|1.9|102.6|100,102,101,120,100,100,100,100,100,103"
+			data = data + "|"
+			data = data + "20.0,-158.0,16.0"
+			data = data + "|"
+			data = data + str(dt.now().strftime('%Y-%m-%d %H:%M:%S'))
+			s.send(data.encode('utf-8'))
 			print("Received data: " + str(data))
-#		conn.close()
-	except serial.error:
+			time.sleep(0.1)
+		conn.close()
+	except Exception as msg:
+		print(msg)
 		print("Surface Controller Disconnected")
-#		conn.close()
+		conn.close()
 
 
 # Call main function
